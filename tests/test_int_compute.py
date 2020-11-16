803bbde46ac8ecbca63bfaff2ed3c3e2939968f9
Stefan Marr <git@stefan-marr.de>
2020-11-16 23:00:07 +0000

    Updated expected code of test
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/test_int_compute.py
+++ tests/test_int_compute.py
@@ -10,43 +10,88 @@ def test_int_comp(tmp_path):
 
     expected_output = """IntComp = (
   base0: a b: b c: c = (
-    ^ c / 4383 % (5564 - c abs + 9) + (c / 2665) * b abs max: a abs % (a - a + 7) % (b + 11)
+    | l1 |
+    l1 := 9.
+    l1 := l1 * c negated.
+    l1 := l1 min: (a min: b) + (a * a).
+    ^ l1
   )
 
-  base1: a b: b = (
-    ^ (b max: 2853) % (a abs * b + 7) - (a - a abs - a)
+  base1: a = (
+    | l1 |
+    l1 := 6.
+    l1 := l1 abs.
+    l1 := l1 min: a + a.
+    l1 := l1 + 387 abs.
+    l1 := l1 * (a / 470).
+    l1 := l1 * (a % 781 / 867).
+    ^ l1
   )
 
-  base2: a b: b = (
-    ^ a - 7567 - b % (966 abs - a * 5205 + 11)
+  base2: a = (
+    | l1 |
+    l1 := 6.
+    l1 := l1 negated.
+    l1 := l1 min: a negated.
+    l1 := l1 negated.
+    l1 := l1 + (a * a).
+    ^ l1
   )
 
-  method0: a b: b = (
-    ^ (self base2: 6966 b: a) / ((self base1: (self base0: a b: b c: b) b: b) + 10)
+  method0: a = (
+    | l1 |
+    l1 := 6.
+    l1 := l1 min: (self base1: a).
+    l1 := l1 * (self base1: 704).
+    l1 := l1 + (self base2: a).
+    ^ l1
   )
 
   method1: a b: b = (
-    ^ (self base2: 2977 b: a) + (self base1: a b: 4365) % ((self base0: 5623 b: b c: b) + 7)
+    | l1 |
+    l1 := 5.
+    l1 := l1 - (self base1: a).
+    l1 := l1 min: (self base0: b b: b c: 658).
+    ^ l1
   )
 
-  method2: a b: b = (
-    ^ self base2: (self base1: (self base0: b b: 5088 c: b) b: b) b: a
+  method2: a = (
+    | l1 |
+    l1 := 10.
+    l1 := l1 * (self base1: a).
+    ^ l1
   )
 
-  method3: a b: b = (
-    ^ (self method2: 3345 b: b) / ((self method1: 6982 b: 8885) + 11) / ((self method0: 2755 b: b) + 10)
+  method3: a = (
+    | l1 |
+    l1 := 3.
+    l1 := l1 min: (self method0: 156).
+    ^ l1
   )
 
-  method4: a b: b = (
-    ^ self method2: (self method1: (self method0: a b: a) b: b) b: b
+  method4: a = (
+    | l1 |
+    l1 := 9.
+    l1 := l1 + (self method2: a).
+    l1 := l1 min: (self method1: a b: a).
+    ^ l1
   )
 
-  method5: a b: b = (
-    ^ (self method2: a b: 9490) / ((self method1: b b: b) + 9) - (self method0: a b: b)
+  method5: a = (
+    | l1 |
+    l1 := 6.
+    l1 := l1 - (self method2: 787).
+    ^ l1
   )
 
   run = (
-    ^ (self method5: (self method4: 937 b: 1662) b: 1840) - (self method3: 7112 b: 6785)
+    | l1 |
+    l1 := 5.
+    l1 := l1 as32BitSignedValue.
+    l1 := l1 min: (self method5: 736).
+    l1 := l1 abs.
+    l1 := l1 - (self method4: (self method3: 464)).
+    ^ l1
   )
 
 )
