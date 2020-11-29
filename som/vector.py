02c27ac3f55f305cc4681c7a7714903f9650732a
Stefan Marr <git@stefan-marr.de>
2020-11-29 00:07:22 +0000

    Avoid producing empty methods
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som/vector.py
+++ som/vector.py
@@ -27,7 +27,7 @@ def create_vector_or_array(values, target_class, factory_name):
         helper_m.add_statement(MsgSend("append:", [Read("vector"), val]))
         stmt_cnt += 1
 
-        if stmt_cnt == get_max_statements_in_method():
+        if stmt_cnt == get_max_statements_in_method() and remaining_values:
             i += 1
             helper_m = _new_helper(factory_m, factory_name, i, target_class, vector)
             stmt_cnt = 0
