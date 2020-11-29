d7ce3d6a33b6113c7d9efc361e21336101cdedcb
Stefan Marr <git@stefan-marr.de>
2020-11-29 18:23:20 +0000

    Started a main.py that’s more immediately useful
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
+import sys
+from argparse import ArgumentParser
+
 from gen.entry_point_gen import EntryPointGenerator
 from gen.int_compute_class_gen import IntegerComputationClassGenerator
 from gen.method_gen import create_method_print_string
 
-## Approach, sketch it out first, don't generalize yet
-
-# Scenario 1
-# - generate a class with a run method that prints hello world
+from gen.spec_reader import SpecificationReader
 
 OUTPUT_DIR = "gen_out"
 
-m_gen = create_method_print_string("run", "Hello World!")
-gen = EntryPointGenerator("Hello", m_gen)
+SPEC_DEFAULT = "/Users/smarr/Projects/SOM/SOM.corelib/specification/index.md"
 
-gen.serialize(OUTPUT_DIR)
+parser = ArgumentParser(description="GigaGenSOM is a SOM source code generator")
+parser.add_argument(
+    "--spec",
+    help="Read SOM specification and generate executable tests.",
+    nargs="?",
+    metavar="spec-file",
+)
 
-int_gen = IntegerComputationClassGenerator("IntComp", 3, 3)
-int_gen.serialize(OUTPUT_DIR)
+parser.add_argument(
+    "--int-comp",
+    help="Generate integer computation code",
+    nargs=2,
+    metavar=("callStackHeight", "numMethods"),
+    type=int,
+)
+
+parser.add_argument(
+    "--hello", help="Generates a Hello World program", action="store_true"
+)
+
+args = parser.parse_args()
+
+if args.hello:
+    m_gen = create_method_print_string("run", None, "Hello World!")
+    gen = EntryPointGenerator("Hello", m_gen)
+    gen.serialize(OUTPUT_DIR)
+
+if args.int_comp:
+    # 3, 10000
+    int_gen = IntegerComputationClassGenerator(
+        "IntComp", args.int_comp[0], args.int_comp[1]
+    )
+    int_gen.serialize(OUTPUT_DIR)
+
+if "--spec" in sys.argv:
+    spec = SpecificationReader(args.spec or SPEC_DEFAULT)
+    spec.read_spec()
+    spec.serialize(OUTPUT_DIR)
